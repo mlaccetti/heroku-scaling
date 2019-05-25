15999d99f0850428568745ce45784aca0c4ee6f2
Michael Laccetti <michael@laccetti.com>
2019-05-25 14:23:11 -0300

    Get our template generation in order

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const viewPath = path.join(__dirname, './views');
 
 const Koa = require('koa');
 var Router = require('koa-router');
@@ -8,8 +7,18 @@ const logger = require('koa-logger');
 const responseTime = require('koa-response-time');
 const views = require('koa-views');
 
+const bcrypt = require('bcrypt');
 const nunjucks = require('nunjucks');
-const nunjucksEnvironment = new nunjucks.Environment(new nunjucks.FileSystemLoader(viewPath));
+
+const viewPath = path.join(__dirname, './views');
+const env = new nunjucks.Environment(new nunjucks.FileSystemLoader(viewPath));
+env.addFilter('json', function(value, spaces) {
+  if (value instanceof nunjucks.runtime.SafeString) {
+    value = value.toString();
+  }
+  const jsonString = JSON.stringify(value, null, spaces).replace(/</g, '\\u003c');
+  return nunjucks.runtime.markSafe(jsonString);
+});
 
 const app = new Koa();
 app.use(logger());
@@ -19,13 +28,23 @@ app.use(
     extension: 'njk',
     map: { njk: 'nunjucks' },
     options: {
-      nunjucksEnv: nunjucksEnvironment
+      nunjucksEnv: env
     }
   })
 );
 
 const router = new Router();
-router.get('/', (ctx) => {
+router.get('/', async (ctx) => {
+  // we do this per-request because we are intentionally trying to create load
+  const saltRounds = 13;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashed = await bcrypt.hash(JSON.stringify(ctx.request.headers), salt);
+
+  ctx.state = {
+    username: 'from-koa',
+    request: ctx.request.headers,
+    hashed: hashed
+  };
   return ctx.render('index.njk');
 });
 
